679db2cb067ad1035b3d9a0637b40640f3836b8b
Jaden Mimura <lesumoracode@gmail.com>
2023-11-13 20:59:40 +0800

    initial commit

---
+++ src/com/enrollment/Main.java
@@ -0,0 +1,8 @@
+package com.enrollment;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RegistrationForm registrationForm = new RegistrationForm(null);
+    }
+}
---
+++ src/com/enrollment/User.java
@@ -0,0 +1,5 @@
+package com.enrollment;
+
+public class User {
+    public String name, email, phone, address, password;
+}
